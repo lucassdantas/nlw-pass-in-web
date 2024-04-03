1c1d5ac906daa515c57e44feead03478867558aa
Lucas Dantas <lucasdantasprogramador@gmail.com>
2024-04-02 22:55:43 -0300

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+import { AtendeeList } from "./components/Attendee-list";
+import { Header } from "./components/Header";
+
 export default function App(){
-  return <h1>teste</h1>
+  return (
+    <div className="flex gap-2">
+      <Header/>
+      <AtendeeList/>
+    </div>
+  )
 }
